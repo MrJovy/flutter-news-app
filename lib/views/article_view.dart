d3b33430c133585b9f330fc1d586265213d547bb
MrJovy <47345645+MrJovy@users.noreply.github.com>
2020-09-13 23:50:28 -0400

    Article View renders properly
    - Clicking on news card from home correctly displays article
    - Note also made some changes for ios
    - Changes to info.plist in ios folder required for ios dev

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 class ArticleView extends StatefulWidget {
+  final String blogUrl;
+  ArticleView({this.blogUrl});
+
   @override
-  _ArticState createState() => _ArticState();
+  _ArticleViewState createState() => _ArticleViewState();
 }
 
-class _ArticState extends State<ArticleView> {
+class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Caribbean"),
+              Text("News", style: TextStyle(color: Colors.blue))
+            ],
+          ),
+          actions: <Widget>[
+            Opacity(
+              opacity: 0,
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Icon(Icons.save)),
+            )
+          ],
+          elevation: 0.0,
+        ),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: WebView(
+              initialUrl: widget.blogUrl,
+              onWebViewCreated: ((WebViewController webViewController) {
+                _completer.complete(webViewController);
+              }),
+            )));
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter_first_test/helper/news.dart';
 import 'package:flutter_first_test/models/article_model.dart';
 import 'package:flutter_first_test/models/category_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_first_test/views/article_view.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -81,10 +82,10 @@ class _HomeState extends State<Home> {
                         physics: ClampingScrollPhysics(),
                         itemBuilder: (context, index) {
                           return BlogTile(
-                            imageUrl: articles[index].urlToImage,
-                            title: articles[index].title,
-                            desc: articles[index].description,
-                          );
+                              imageUrl: articles[index].urlToImage,
+                              title: articles[index].title,
+                              desc: articles[index].description,
+                              blogUrl: articles[index].url);
                         },
                       ),
                     )
@@ -141,31 +142,44 @@ class CategoryTile extends StatelessWidget {
 }
 
 class BlogTile extends StatelessWidget {
-  final String imageUrl, title, desc;
+  final String imageUrl, title, desc, blogUrl;
   BlogTile(
-      {@required this.imageUrl, @required this.title, @required this.desc});
+      {@required this.imageUrl,
+      @required this.title,
+      @required this.desc,
+      @required this.blogUrl});
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(bottom: 16),
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: Image.network(imageUrl)),
-            SizedBox(
-              height: 8,
-            ),
-            Text(title,
-                style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.w500)),
-            SizedBox(
-              height: 8,
-            ),
-            Text(desc, style: TextStyle(color: Colors.black45)),
-          ],
-        ));
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticleView(
+                      blogUrl: blogUrl,
+                    )));
+      },
+      child: Container(
+          margin: EdgeInsets.only(bottom: 16),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(imageUrl)),
+              SizedBox(
+                height: 8,
+              ),
+              Text(title,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500)),
+              SizedBox(
+                height: 8,
+              ),
+              Text(desc, style: TextStyle(color: Colors.black45)),
+            ],
+          )),
+    );
   }
 }
